b2ba2edfb401ae60a91a6fac4769fb6404342302
imaz <neko.daisuki.070109@gmail.com>
2011-10-08 02:10:16 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gettan::Application.config.secret_token = '6b463b9429f269b3455ef6ae68c08bf3d478b588b5811cb78ed41d39f512f9c3f0b504408afe9242b9e35b6de438f286f3b82e1252e132a4b5f22ae848a85c7d'
