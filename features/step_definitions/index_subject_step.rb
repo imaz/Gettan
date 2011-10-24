d37cfde8617531e72dcf990b5a4bafd81ac37f36
imaz <neko.daisuki.070109@gmail.com>
2011-10-25 01:57:22 +0900

    [feature] 科目一覧を表示する のテストを追加

---
+++ features/step_definitions/index_subject_step.rb
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+# language: ja
+
+前提 /^システムの "([^"]*)" が表示されている$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし /^ユーザは "([^"]*)" のリンクをクリックする$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば /^システムは "([^"]*)" ページを表示する$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
