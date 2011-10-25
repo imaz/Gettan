3b57cc1f499b530c9493bc170653ad02c472868a
imaz <neko.daisuki.070109@gmail.com>
2011-10-25 09:41:21 +0900

    [feature] ページ表示のステップを実装

--- features/step_definitions/index_subject_step.rb
+++ features/step_definitions/index_subject_step.rb
@@ -1,8 +1,8 @@
 # encoding: UTF-8
 # language: ja
 
-前提 /^システムの "([^"]*)" が表示されている$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+前提 /^システムの "([^"]*)" が表示されている$/ do |page_name|
+	visit path_to(page_name)
 end
 
 もし /^ユーザは "([^"]*)" のリンクをクリックする$/ do |arg1|
